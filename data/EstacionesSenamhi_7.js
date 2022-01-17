25ca8dfe7ab67392b374e5dd91dbc1103c95eeff
pattyqr <patricia.quevedo@unmsm.edu.pe>
2022-01-17 14:13:20 -0500

    mapa

---
+++ data/EstacionesSenamhi_7.js
@@ -0,0 +1,24 @@
+var json_EstacionesSenamhi_7 = {
+"type": "FeatureCollection",
+"name": "EstacionesSenamhi_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ESTACION": "Campo de Marte", "DEP": "Lima", "PROV": "Lima", "DIST": "Jesus Maria", "ALT": "117", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -77.043138633083188, -12.070563472972387 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Santa Anita", "DEP": "Lima", "PROV": "Lima", "DIST": "Santa Anita", "ALT": "253", "TIPO": "Automatica-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.971444635606645, -12.043022634522407 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "�a�a", "DEP": "Lima", "PROV": "Lima", "DIST": "Lurigancho", "ALT": "543", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.841944895934787, -11.98744452028933 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Chosica", "DEP": "Lima", "PROV": "Lima", "DIST": "Lurigancho", "ALT": "867", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.689746958903143, -11.929855981033967 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Santa Eulalia", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Santa Eulalia", "ALT": "970", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.666638991474997, -11.920000170685187 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "San Mateo de Otao", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "San Mateo de Otao", "ALT": "3506", "TIPO": "Automatica-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.563614185988811, -11.846999812099801 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Matucana", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Matucana", "ALT": "2417", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.378028149720663, -11.839127636451815 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "San Jose de Parac", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "San Mateo", "ALT": "3829", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.258111406450169, -11.800524753935344 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "San Mateo de Huanchor", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "San Mateo", "ALT": "3155", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.301000093115817, -11.760166338963133 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Rio Blanco", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Chicla", "ALT": "3503", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.259666400558842, -11.734466866798462 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Casapalca", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Chicla", "ALT": "4294", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.23332222463705, -11.63803862110602 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Casapalca", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Chicla", "ALT": "4294", "TIPO": "Automatica-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.233249594633136, -11.638000052818695 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Autisha", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "San Antonio", "ALT": "2220", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.611027493464064, -11.738147499098053 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Carampoma", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Carampoma", "ALT": "3424", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.51513618052131, -11.655027905406708 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Sheque", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Carampoma", "ALT": "3188", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.502300060213784, -11.660950005627539 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Tingo", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Huanza", "ALT": "3980", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.483336166027286, -11.616672536215804 ] } },
+{ "type": "Feature", "properties": { "ESTACION": "Milloc", "DEP": "Lima", "PROV": "Huarochiri", "DIST": "Carampoma", "ALT": "4384", "TIPO": "Convencional-Meteorologica" }, "geometry": { "type": "Point", "coordinates": [ -76.349947638126238, -11.571307974346801 ] } }
+]
+}
